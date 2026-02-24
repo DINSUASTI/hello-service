df2d579d3aaeb92929b4e32419ed35afd43d784d
David Insuasti <davidinsuasti@gmail.com>
2026-02-24 17:02:18 -0500

    Creación del repo

---
+++ src/main/java/com/demo/hello_ws/HelloController.java
@@ -0,0 +1,12 @@
+package com.demo.hello_ws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hola Mundo!";
+    }
+}
