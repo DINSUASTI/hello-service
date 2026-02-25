fbafa03a212cd9dc7921407ec34d8c464689a8e9
DINSUASTI <davidinsuasti@gmail.com>
2026-02-25 17:05:06 -0500

    Hola Mundo! con CI/CD: 16

--- src/main/java/com/demo/hello_ws/HelloController.java
+++ src/main/java/com/demo/hello_ws/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     }
     @GetMapping("/hello")
     public String hello() {
-        return "Hola Mundo! con CI/CD: 14";
+        return "Hola Mundo! con CI/CD: 16";
     }
 }
