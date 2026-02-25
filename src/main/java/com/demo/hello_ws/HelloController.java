e5ef581731c420f87240d89dcfb50a428155e958
DINSUASTI <davidinsuasti@gmail.com>
2026-02-25 12:01:28 -0500

    Hola Mundo! con CI/CD, corregido error runner.2

--- src/main/java/com/demo/hello_ws/HelloController.java
+++ src/main/java/com/demo/hello_ws/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hola Mundo! con CI/CD, corregido error runner.";
+        return "Hola Mundo! con CI/CD, corregido error runner.2";
     }
 }
