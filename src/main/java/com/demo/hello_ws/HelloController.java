3602c9ef313e4d55643ea6b7ae045af0b7237fdc
DINSUASTI <davidinsuasti@gmail.com>
2026-02-25 16:45:13 -0500

    Hola Mundo! con CI/CD: 14

--- src/main/java/com/demo/hello_ws/HelloController.java
+++ src/main/java/com/demo/hello_ws/HelloController.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/")
+    public String inicio() {
+        return "Welcome";
+    }
+    @GetMapping("/hello")
     public String hello() {
-        return "Hola Mundo! con CI/CD: 13";
+        return "Hola Mundo! con CI/CD: 14";
     }
 }
